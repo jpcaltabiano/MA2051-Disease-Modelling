ea75585aa8b897870bc1fe095ab5205bcd3d5d2f
joseph <jpcaltabiano@wpi.edu>
2018-11-16 01:00:59 -0500

    initial commit

---
+++ test-model-script.cpp
@@ -0,0 +1,20 @@
+/*
+ * Joseph Caltabiano Nov 2018
+ *
+ * Model:
+ * Create a population of indivduals and model the spread of disease throughout them
+ * Individuals could have certain charcateristics, such as 
+ * 	- Suceptible, recovered, and infected
+ * 	- Age
+ * The model would have certain paramters such as 
+ *	- the contact rate of individuals
+ * 	- death rate of individuals
+ *  - birth rate of new individuals
+ *
+ * Purpose:
+ * This script would model an implementation of the SIR model for the spread of
+ * infetcious diseases. User can change input to change different variables in the model
+ * which would reflect in the output. Output would be a list of statisttics about the 
+ * events that occured during the model and final population statistics
+ */
+
